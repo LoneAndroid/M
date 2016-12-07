5ddd23ea5f00088c47d71ce0b2fb38ea72e9e9ad
LoneAndroid <gayduk.matwey@gmail.com>
2016-12-07 18:53:55 +0300

    Вроде работает
    Оччччччень много.

---
+++ app/src/main/java/weaponcenter/rubananaweaponsweaponstore/Adapturus.java
@@ -0,0 +1,44 @@
+package weaponcenter.rubananaweaponsweaponstore;
+
+import android.content.Context;
+import android.media.Image;
+import android.support.annotation.NonNull;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import static java.security.AccessController.getContext;
+import static weaponcenter.rubananaweaponsweaponstore.R.id.textView;
+
+/**
+ * Created by LoneAndroid on 11/6/1918.
+ */
+
+public class Adapturus extends ArrayAdapter {
+
+    public Adapturus(Context context, int resource, Object[] objects) {
+        super(context, resource, objects);
+    }
+
+    @Override
+    public View getView(int position, View convertView, @NonNull ViewGroup parent) {
+        // создаём элемент для geirb с фотографией и двумя nhtvz xtnshtvz полями gjkzvb
+        View view = LayoutInflater.from(getContext()).inflate(R.layout.weapon_item, parent, false);
+
+        Weapon weapon = (Weapon) getItem(position);
+
+        TextView textView = (TextView) view.findViewById(R.id.textView2);
+        textView.setText(weapon.model);
+
+        ImageView imageView = (ImageView) view.findViewById(R.id.imageView2);
+        imageView.setImageResource(weapon.ex);
+
+        // возращаем созданный элементик
+        return view;
+    }
+
+}
--- app/src/main/java/weaponcenter/rubananaweaponsweaponstore/MainActivity.java
+++ app/src/main/java/weaponcenter/rubananaweaponsweaponstore/MainActivity.java
@@ -20,37 +20,41 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        оружие = new Weapon[5];
+        оружие = new Weapon[23];
         оружие[0] = new Weapon("Винчестер 'Шерлок'", 450, R.drawable.w1);
         оружие[1] = new Weapon("Сверчек Офисный", 50, R.drawable.w2);
         оружие[2] = new Weapon("Турболазер 'Ледокол'", 1685, R.drawable.w3);
         оружие[3] = new Weapon("АнтиДалек 'ДокторКто'", 1200, R.drawable.w4 );
         оружие[4] = new Weapon("Н-винтовка Мк1+Мк2", 4250, R.drawable.w5);
-//        оружие[5] = new Weapon("");
-//        оружие[6] = new Weapon("");
-//        оружие[7] = new Weapon("");
-//        оружие[8] = new Weapon("");
-//        оружие[9] = new Weapon("");
-//        оружие[10] = new Weapon("");
-//        оружие[11] = new Weapon("");
-//        оружие[12] = new Weapon("");
-//        оружие[13] = new Weapon("");
-//        оружие[14] = new Weapon("");
-//        оружие[15] = new Weapon("");
-//        оружие[16] = new Weapon("");
-//        оружие[17] = new Weapon("");
-//        оружие[18] = new Weapon("");
-//        оружие[19] = new Weapon("");
-//        оружие[20] = new Weapon("");
-//        оружие[21] = new Weapon("");
-//        оружие[22] = new Weapon("");
+        оружие[5] = new Weapon("АнтиДалек фермерский", 125, R.drawable.w6);
+        оружие[6] = new Weapon("Лазерная Штурмовая/окопная винтовка ", 1245, R.drawable.w7);
+        оружие[7] = new Weapon("Футур Револьвер", 680, R.drawable.w8);
+        оружие[8] = new Weapon("GN-808", 1100, R.drawable.w9);
+        оружие[9] = new Weapon("Снайперский турболазер", 1850, R.drawable.w10);
+        оружие[10] = new Weapon("ОФИ 'Оффи'", 950, R.drawable.w11);
+        оружие[11] = new Weapon("XTRZ-435149", 860, R.drawable.w12);
+        оружие[12] = new Weapon("CDI 'Контрабандист'", 735, R.drawable.w13);
+        оружие[13] = new Weapon("Плазменная пушка 'Makarov'", 2400, R.drawable.w14);
+        оружие[14] = new Weapon("Пучковый бластер+Пульсар", 2055, R.drawable.w15);
+        оружие[15] = new Weapon("Лазерная снайперская винтовка", 1780, R.drawable.w16);
+        оружие[16] = new Weapon("Плазменная снайперская винтовка 'SVD'", 5200, R.drawable.w17);
+        оружие[17] = new Weapon("Гвардейский клинок Литии", 8000, R.drawable.s1);
+        оружие[18] = new Weapon("Клинок Древних Родонтинов- рассы первого поколения", 120000, R.drawable.s2);
+        оружие[19] = new Weapon("Кристальный клинок АстроШумеров", 4500, R.drawable.s3);
+        оружие[20] = new Weapon("Клинок СпэйсЭльфов", 9300, R.drawable.s4);
+        оружие[21] = new Weapon("Меч Темной материи", 50000, R.drawable.s5);
+        оружие[22] = new Weapon("Шокер для Крысолюдов", 20, R.drawable.s6);
 
         ListView listView = (ListView) findViewById(R.id.Табличище);
         int itemLayout = android.R.layout.simple_list_item_1;
-        ArrayAdapter adapter = new ArrayAdapter<Weapon>(this, itemLayout, оружие);
+        Adapturus adapter = new Adapturus(this, itemLayout, оружие);
         listView.setAdapter(adapter);
+
+
+
+
+        }
 
 
     }
 
-}
